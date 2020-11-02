f3156fabc9b2e6c6c443746400b68f1779dd5fe8
SamNel2000 <73793403+SamNel2000@users.noreply.github.com>
2020-11-01 20:48:46 -0600

    Junior Year

---
+++ Root Calculator.py
@@ -0,0 +1,7 @@
+#Root Calculator, had this idea while on a run on May 23rd, 2020
+num = int(input("Enter the value you wish to find the square root of: "))
+on = True
+length = len(str(num))
+#while(on):
+for a in range(0, int(num/length)):
+   print(a)
